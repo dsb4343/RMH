4d3340f77e2a76c997824cb00aefa9664d96b85b
Bryan Weikle <weikle78@ksu.edu>
2019-03-21 13:40:19 -0500

    3/21/19 Added guest and patient objects

--- objects/Patient.js
+++ objects/Patient.js
@@ -2,43 +2,15 @@
 var mongoose = require('mongoose');
 
 //Schema
-var Schema = mongoose.Schem;
+var Schema = mongoose.Schema;
 
 //Object
-var PersonSchema = new Schema({
-    lastName: {
-        type: String,
-        max: 100
-
-    },
-    phone: {
-        type: Number,
-        max: 10
-    },
-    email: {
-        type: String,
-    },
-    street: {
-        type: String,
-    },
-    city: {
-        type: String,
-    },
-    state: {
-        type: String,
-    },
-    zip: {
-        type: String,
-    },
-    emergencyContact: {
-        type: String,
-    },
-    emergencyPhone: {
-        type: String,
-    }
+var PatientSchema = new Schema({
+    patientLocation: {type: String, max:100}
 });
 
 //Export for mongoose
-module.exports = mongoose.model('Person', PersonSchema);
+module.exports = mongoose.model('Patient', PatientSchema);
 
 //Virtuals
+
