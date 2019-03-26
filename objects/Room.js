3a326b8c335dbac9db9a8aec5380550964ecbc96
Lab User <LabUser@labs.salina.k-state.edu>
2019-03-26 13:29:33 -0500

    3/26

---
+++ objects/Room.js
@@ -0,0 +1,25 @@
+//Requires
+var mongoose = require('mongoose');
+
+//Schema
+var Schema = mongoose.Schema;
+
+//Object
+var RoomSchema = new Schema({
+    lastName: {type: String, max: 100},
+    firstName: {type: String, max: 100},
+    phone: {type: Number, max: 10},
+    email: {type: String, },
+    street: {type: String, },
+    city: {type: String, },
+    state: {type: String, },
+    zip: {type: String, },
+    emergencyContact: {type: String, },
+    emergencyPhone: {type: String, }
+});
+
+//Export for mongoose
+module.exports = mongoose.model('Person', PersonSchema);
+
+//Virtuals
+
