3d264e18ad48c5918d8a824138cb4447a0c2b7b0
Lab User <LabUser@labs.salina.k-state.edu>
2019-04-23 14:18:05 -0500

    Finished 'Staff' controller 4/23

--- controllers/staffController.js
+++ controllers/staffController.js
@@ -31,17 +31,32 @@ exports.staff_read = function(req, res) {
 
 // Display staff create form on GET.
 exports.staff_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: staff create GET');
+    Staff.findById(req.params.id)
+    .execute(function (err, results){
+        if (err) {return next(err);}
+        if (results==null) {
+            res.redirect('/staff/create');
+        }
+        console.log(results);
+        res.render('staff_create', {
+            title: 'New Staff',
+            staff: staff
+        });
+    });
+    
+    //res.send('NOT IMPLEMENTED: staff create GET');
 };
 
 // Handle staff create on POST.
 exports.staff_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: staff create POST');
+    //res.send('NOT IMPLEMENTED: staff create POST');
   //Validation
   body('lastName').isLength({min: 1}).trim().withMessage('Missing Last Name'),
   body('firstName').isLength({min: 1}).trim().withMessage('Missing First Name'),
+  body('email').isLength({min: 1}).trim().withMessage('Missing Email Address'),
   body('phone').isLength({min: 1}).trim().withMessage('Missing Phone Number'),
   sanitizeBody('lastName').trim().escape(),
+  sanitizeBody('email').trim().escape(),
   sanitizeBody('firstName').trim().escape(),
   sanitizeBody('phone').trim().escape(),
 
@@ -74,20 +89,78 @@ exports.staff_create_post = function(req, res) {
 
 // Display staff delete form on GET.
 exports.staff_delete_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: staff delete GET');
+    Staff.findById(req.params.id)
+    .populate('staff')
+    .execute(function(err,results) {
+        if (err) {return next(err)};
+        if (results==null) {res.redirect('/staff')};
+        res.render('staff_delete', {
+            title: 'Delete Staff',
+            staff: results
+        });
+    });
+    
+    //res.send('NOT IMPLEMENTED: staff delete GET');
 };
 
 // Handle staff delete on POST.
 exports.staff_delete_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: staff delete POST');
+    Staff.findByIdAndDelete(req.params.id, 
+        function deleteStaff(err) {
+        if (err) return next(err);
+        //redirect will need updated url address--------------
+        res.redirect('/users/staff');
+    });
+    
+    //res.send('NOT IMPLEMENTED: staff delete POST');
 };
 
 // Display staff update form on GET.
 exports.staff_update_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: staff update GET');
+    Staff.findById(req.params.id)
+    .populate('staff')
+    .exec(callback);
+    if (err) {return next(err)};
+    res.render('staff_update', {
+        title: 'Update Staff',
+        staff: results.staff
+    });
+    
+    //res.send('NOT IMPLEMENTED: staff update GET');
 };
 
 // Handle staff update on POST.
 exports.staff_update_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: staff update POST');
+     //Validation
+  body('lastName').isLength({min: 1}).trim().withMessage('Missing Last Name'),
+  body('firstName').isLength({min: 1}).trim().withMessage('Missing First Name'),
+  body('email').isLength({min: 1}).trim().withMessage('Missing Email Address'),
+  body('phone').isLength({min: 1}).trim().withMessage('Missing Phone Number'),
+  sanitizeBody('lastName').trim().escape(),
+  sanitizeBody('email').trim().escape(),
+  sanitizeBody('firstName').trim().escape(),
+  sanitizeBody('phone').trim().escape(),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        res.render('staff_update', {
+            title: 'Update Staff Failed',
+            _id: staff._id,
+            staff: staff,
+            errors: errors.array(),
+        });
+    return;
+    }
+    else {
+        var staff = new Staff({
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            phone: req.body.phone,
+    
+        });
+    };
+}  
+    //res.send('NOT IMPLEMENTED: staff update POST');
 };
