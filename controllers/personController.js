d3177c7c586565f1c3afee50cfc455eea7c36350
Lab User <LabUser@labs.salina.k-state.edu>
2019-05-10 09:42:08 -0500

    5/10

--- controllers/personController.js
+++ controllers/personController.js
@@ -201,3 +201,21 @@ exports.person_update_post = [
     }
     //res.send('NOT IMPLEMENTED: person update POST');
 ];
+
+//Search for returning guest
+exports.person_search = (function myFunction() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("Person");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+});
