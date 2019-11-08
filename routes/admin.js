2186f4e26765e4ab98400ec211b93934a83458e6
Lab User <LabUser@labs.salina.k-state.edu>
2019-11-08 13:55:51 -0600

    11/8/19

--- routes/admin.js
+++ routes/admin.js
@@ -51,9 +51,9 @@ router.get('/Registration/create', Registration.registration_create_get);
 
 router.post('/Registration/create', Registration.registration_create_post);
 
-router.get('/Registration//:id/create', Registration.register_guest_get);
+router.get('/Registration//:id/create', Registration.registration_guest_get);
 
-router.post('/Registration/:id/create', Registration.register_guest_post);
+router.post('/Registration/:id/create', Registration.registration_guest_post);
 
 router.get('/Registration/:id/delete', Registration.registration_delete_get);
 
